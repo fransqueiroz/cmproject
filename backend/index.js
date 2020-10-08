88aa9398d828a390c20834f9d3c87b45bbff1c4d
fransqueiroz <fransergiosq@gmail.com>
2020-10-08 18:45:37 -0300

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
-const app = require('express')();
+const app = require('express')()
 const consign = require('consign')
+const db = require('./config/db')
+const mongoose = require('mongoose')
 
-const db = require('./config/db.js');
-app.db = db;
+require('./config/mongodb')
+
+app.db = db
+app.mongoose = mongoose
 
 consign()
-    .then('./config/middewares.js')
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
     .then('./api/validation.js')
     .then('./api')
+    .then('./schedule')
     .then('./config/routes.js')
     .into(app)
 
 app.listen(3000, () => {
     console.log('Backend executando')
 })
-
-
-
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,17 +1,13 @@
-// Update with your config settings.
+const { db } = require('./.env')
 
 module.exports = {
-    client: 'postgresql',
-    connection: {
-      database: 'conhecimento_noite',
-      user:     'postgres',
-      password: 'senha'
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-};
+  client: 'postgresql',
+  connection: db,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+}
